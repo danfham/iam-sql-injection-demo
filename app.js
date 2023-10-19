3ea3419a3f31ae75ca4a2eedf4e8750e4a623efb
danfham <dan.cunningham91@icloud.com>
2023-10-19 09:27:12 -0400

    created lightweight app to test authentication and vulnerabilities in user input based on the query

--- app.js
+++ app.js
@@ -14,8 +14,8 @@ app.use(bodyParser.json())
 //db creation 
 const db = new sqlite3.Database(':memory:');
 db.serialize(function () {
- db.run(“CREATE TABLE user (username TEXT, password TEXT, title TEXT)“);
- db.run(“INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')“);
+ db.run("CREATE TABLE user (username TEXT, password TEXT, title TEXT)");
+ db.run("INSERT INTO user VALUES ('privilegedUser', 'privilegedUser1', 'Administrator')");
 });
 
 app.get('/', function(req,res){
